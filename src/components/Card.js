106105d18b17b6c430b9b45f3bb9480eb7c53853
Diana Calderon <calderon.calx@gmail.com>
2022-06-04 11:07:20 -0600

    Show the information of tasks in the component Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getTechColor } from '../helpers/getTechColor';
+import { getColorToDate, getDate } from '../helpers/getAllAboutDate';
+import { ReactComponent as Clip } from '../assets/clip.svg';
+import { ReactComponent as Diagram } from '../assets/diagram.svg';
+import { ReactComponent as Comment } from '../assets/comment.svg';
+import { ReactComponent as Ellipsis } from '../assets/ellipsis.svg';
+import { ReactComponent as Clock } from '../assets/clock.svg';
+import { splitWords } from '../helpers/splitWords';
+import estimates from '../helpers/estimates';
 
-const Card = ({ task }) => {
-  const { name } = task;
-  return <div>{name}</div>;
-};
+import './Card.scss';
+
+function Card({ task, layout }) {
+  const { name, estimate, dueDate, tags, user, id } = task;
+
+  const initials = splitWords(user.fullName);
+
+  const updateSelection = () => {
+    console.log('Selection');
+  };
+
+  return (
+    <>
+      {layout === 'grid' && (
+        <div className="card">
+          <div className="card__title">
+            <p className="card_p">{name}</p>
+            <Ellipsis id={`task-${id}`} onClick={updateSelection} className="card__ellipsis" />
+          </div>
+
+          <div className="card__data">
+            <p className="card_p">{estimates.find(est => est.value === estimate).name}</p>
+            <div className={`card__date ${getColorToDate(dueDate)}`}>
+              <Clock />
+              <p className="card_p">{getDate(dueDate)}</p>
+            </div>
+          </div>
+
+          <div className="card__labels">
+            {tags.map(label => (
+              <div key={label} className="card__tech">
+                <div style={{ backgroundColor: getTechColor(label), opacity: 0.3 }} />
+                <p className="card_p" style={{ color: getTechColor(label) }}>
+                  {label}
+                </p>
+              </div>
+            ))}
+          </div>
+
+          <div className="card__identifier">
+            <p className="card__iniciales card_p">{initials}</p>
+            <div className="card__icons">
+              <Clip />
+              <div className="card__icon">
+                <p className="card_p">5</p>
+                <Diagram />
+              </div>
+              <div className="card__icon">
+                <p className="card_p">3</p>
+                <Comment />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {layout === 'list' && (
+        <div className="card__list">
+          <div className="card__list__title border">
+            <p className="card_p">{name}</p>
+            <div className="card__icons__list">
+              <div className="card__icon">
+                <p className="card_p">5</p>
+                <Diagram />
+              </div>
+              <div className="card__icon">
+                <p className="card_p">3</p>
+                <Comment />
+              </div>
+            </div>
+          </div>
+          <div className="card__labels__list border">
+            <div className="card__tech">
+              <div style={{ backgroundColor: getTechColor(tags[0]), opacity: 0.3 }} />
+              <p className="card_p" style={{ color: getTechColor(tags[0]) }}>
+                {tags[0]}
+              </p>
+            </div>
+            {tags.length > 1 && <div className="card__quantity">+{tags.length - 1}</div>}
+          </div>
+
+          <div className="card__data__list border">
+            <p className="card_p">{estimates.find(est => est.value === estimate).name}</p>
+          </div>
+
+          <div className="card__identifier__list border">
+            <p className="card__iniciales card_p">{initials}</p>
+            <p>{user.fullName}</p>
+          </div>
+
+          <div className="card__identifier__list border">
+            <div className={`card_p ${getColorToDate(dueDate)}`}>{getDate(dueDate)}</div>
+          </div>
+
+          <div className="card__button__ellipsis">
+            <Ellipsis id={`task-${id}`} onClick={updateSelection} className="card__ellipsis" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
 
 export default Card;
