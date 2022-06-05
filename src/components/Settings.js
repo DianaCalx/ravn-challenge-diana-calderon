ac39ac87332372eb74c722f7e79db189f97b0e2e
Diana Calderon <calderon.calx@gmail.com>
2022-06-05 08:06:00 -0600

    Fetch profile data from the API in settings view

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import { splitWords } from '../helpers/splitWords';
 import { getDate } from '../helpers/getAllAboutDate';
+import { getProfile } from '../graphql/queries';
 import './Settings.scss';
-import myJson from '../data.json';
 
 const Settings = () => {
-  const { user } = myJson;
+  const { data, loading, error } = useQuery(getProfile);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>There was an error</div>;
+  }
+
+  const { avatar, fullName, type, email, createdAt, updatedAt } = data.profile;
 
   return (
     <div className="settings">
       <div className="settings__card">
-        <div className="settings__itials">{splitWords(user.fullName)}</div>
-        <p className="settings__fullName settings__data">{user.fullName}</p>
-        <p className="settings__type">{user.type}</p>
-        <p className="settings__data">{user.email}</p>
+        {avatar ? <img alt="User avatar" src={avatar} /> : <div className="settings__itials">{splitWords(fullName)}</div>}
+        <p className="settings__fullName settings__data">{fullName}</p>
+        <p className="settings__type">{type}</p>
+        <p className="settings__data">{email}</p>
         <p className="settings__data">
           Created at:
-          <span>{getDate(user.createdAt)}</span>
+          <span> {getDate(createdAt)}</span>
         </p>
         <p className="settings__data">
           Updated at:
-          <span>{getDate(user.updatedAt)}</span>
+          <span> {getDate(updatedAt)}</span>
         </p>
       </div>
     </div>
