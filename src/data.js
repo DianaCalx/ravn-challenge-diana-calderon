43b09143c8f87220efe7344fcbd1e79fa97b7965
Diana Calderon <calderon.calx@gmail.com>
2022-06-05 10:28:25 -0600

    Move fields data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = {
+  estimates: [
+    {
+      name: '0 Points',
+      value: 'ZERO',
+    },
+    {
+      name: '1 Points',
+      value: 'ONE',
+    },
+    {
+      name: '2 Points',
+      value: 'TWO',
+    },
+    {
+      name: '4 Points',
+      value: 'FOUR',
+    },
+    {
+      name: '8 Points',
+      value: 'EIGHT',
+    },
+  ],
+  tags: ['ANDROID', 'IOS', 'NODE_JS', 'RAILS', 'REACT'],
+  status: ['BACKLOG', 'TODO', 'IN_PROGRESS', 'CANCELLED', 'DONE'],
+};
+
+export default data;
